246f469789b480f090483b8a6ba50751f35146dd
dev <dev@MacBook-Pro-4.local>
2021-08-09 09:27:20 -0700

    Fixed after login redirect path

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/conversations';
 
     /**
      * The controller namespace for the application.
